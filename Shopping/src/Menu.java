47978bb8b6c1c031ba7d375f29c93961cb342107
RamanaChandraLekha <sravyamamidi.06@gmail.com>
2017-03-08 18:28:01 +0530

    Update Menu.java

--- Shopping/src/Menu.java
+++ Shopping/src/Menu.java
@@ -1,9 +1,14 @@
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Iterator;
 import java.util.Set;
 
 public class Menu {
+	BufferedReader read=new BufferedReader(new InputStreamReader(System.in));
 	boolean flag=false;
+	String choice;
 	public int availableItems(int available, int required) throws RuntimeException {
 		if (required <= available) {
 			available = available - required;
@@ -49,5 +54,23 @@ public class Menu {
 		else
 			throw new RuntimeException();
 	}
+	public String inputCheck() throws IOException {
+
+		String choice1 = read.readLine();
+		do{
+		switch (choice1) {
+		case "yes":
+		choice = choice1;
+		break;
+		case "no":choice=choice1;
+		break;
+		default:
+		System.out.println("enter valid input(yes/no)");
+		choice = read.readLine();
+		break;
+		}}while(!(choice.equalsIgnoreCase("yes"))&&!(choice.equalsIgnoreCase("no")));
+
+		return choice;
+		}
 	
 }
